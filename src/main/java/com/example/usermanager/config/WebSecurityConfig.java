dd2544d4f1b0904855db2e5bb37cbc62abfee712
lhoang1910 <hoang03.it@gmail.com>
2024-06-21 12:24:41 +0700

    update nhieu ti

--- src/main/java/com/example/usermanager/config/WebSecurityConfig.java
+++ src/main/java/com/example/usermanager/config/WebSecurityConfig.java
@@ -26,6 +26,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/**").hasAuthority(UserRole.ADMIN.name())
                         .requestMatchers("/auth/v1/**").permitAll()
